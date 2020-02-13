e61183bee673de08217731dc77678d25fd21ce81
reubengt <reubengeorgethomas@gmail.com>
2020-02-13 19:25:03 +0000

    add cookieParser and router to express app

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 const express = require("express");
+const router = require("./router");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.post("/api/createuser", (req, res) => {
-  console.log(req.body);
-});
-
-app.post("/api/login", (req, res) => {});
+app.use(cookieParser());
+app.use(router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+app.listen(5000, () => console.log(`Server started on port ${port}`));
