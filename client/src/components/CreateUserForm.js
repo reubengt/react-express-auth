9d4e29aa28f5cc7f6ea636dde764fdbd82dd9083
reubengt <reubengeorgethomas@gmail.com>
2020-02-14 22:41:06 +0000

    add state for form fields, error message, and make custom call to api to create user

--- client/src/components/CreateUserForm.js
+++ client/src/components/CreateUserForm.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import sendUserData from "../helpers/sendUserData";
 const CreateUserForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const handleUsernameChange = e => setUsername(e.target.value);
+  const handlePasswordChange = e => setPassword(e.target.value);
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const response = await sendUserData("/api/createuser", {
+      username,
+      password
+    });
+    const responseJson = await response.json();
+    if (response.status !== 201) {
+      setError(responseJson.error);
+    } else {
+      console.log("creating user successful");
+    }
+  };
+
   return (
-    <form
-      class="form create-form"
-      autocomplete="off"
-      action="/api/createuser"
-      method="post"
-    >
+    <form class="form create-form" autocomplete="off" onSubmit={handleSubmit}>
       <label for="new-username">Create a new username</label>
       <br />
       <input
@@ -16,8 +32,10 @@ const CreateUserForm = () => {
         placeholder="username"
         required
         pattern="[A-za-z0-9]+"
+        value={username}
+        onChange={handleUsernameChange}
       ></input>
-      <p id="new-username-err"></p>
+      <p id="new-username-err">{error}</p>
       <input
         type="password"
         name="password"
@@ -27,8 +45,10 @@ const CreateUserForm = () => {
         minlength="6"
         pattern="[A-za-z0-9]+"
         required
+        value={password}
+        onChange={handlePasswordChange}
       ></input>
-      <p id="new-password-err"></p>
+      <p id="new-password-err">{error}</p>
       <input type="submit" value="GO"></input>
     </form>
   );
